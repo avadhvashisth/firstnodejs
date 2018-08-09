7c3edf51c725197ec4c240b04ce721cc685f19b1
avadhvashisth <avadh.vashisth@gmail.com>
2018-08-10 01:47:53 +0530

    adding database

--- app.js
+++ app.js
@@ -11,7 +11,29 @@ app.use(bodyParser.json());
 var port = process.env.PORT || 8090;
 var router = express.Router();
 
-mongoose.connect('');
+mongoose.connect('mongodb://Avadh vashisth:avadh.5868@ds113906.mlab.com:13906/firstnodejs');
+
+router.use(function (req, res, next) {
+
+		router.route('/products').post(function (req, res) {
+		    console.log("in add");
+		    var p = new product();
+		    p.title = req.body.title;
+		    p.price = req.body.price;
+		    p.instock = req.body.instock;
+		    p.photo = req.body.photo;
+		    p.save(function (err) {
+		        if (err) {
+		            res.send(err);
+		        }
+		        console.log("added");
+		        res.send({ message: 'Product Created !' });
+		    });
+		});
+
+	console.log('Logging of request will be done here');
+	next();
+});
 
 app.use(cors());
 app.use("/api", router);
--- product.js
+++ product.js
@@ -1,8 +1,10 @@
-var mongoos = require('mongoose');
+var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var ProductSchema = new Schema({
 	title: String,
-	Price: Number,
+	price: Number,
 	instock: Boolean,
 	photo: String,
 });
+
+module.exports = mongoose.model('Product', ProductSchema);
