b5e643e43a9130576b5ee87407c1be6f9bd19fb4
avadhvashisth <33063796+avadhvashisth@users.noreply.github.com>
2018-08-10 22:56:28 +0530

    now working

--- app.js
+++ app.js
@@ -11,28 +11,32 @@ app.use(bodyParser.json());
 var port = process.env.PORT || 8090;
 var router = express.Router();
 
-mongoose.connect('mongodb://Avadh vashisth:avadh.5868@ds113906.mlab.com:13906/firstnodejs');
 
-router.use(function (req, res, next) {
+mongoose.connect('mongodb://first:avadh123@ds113906.mlab.com:13906/firstnodejs',function(err,e){
+    if(err) return console.log(err);
+    console.log('connected!!!');
+});
 
-		router.route('/products').post(function (req, res) {
-		    console.log("in add");
-		    var p = new product();
-		    p.title = req.body.title;
-		    p.price = req.body.price;
-		    p.instock = req.body.instock;
-		    p.photo = req.body.photo;
-		    p.save(function (err) {
-		        if (err) {
-		            res.send(err);
-		        }
-		        console.log("added");
-		        res.send({ message: 'Product Created !' });
-		    });
-		});
+router.get('/',function(req,res){
+	return res.json({'js':"ajsna"});
+})
+		
+router.route('/products').post(function (req, res) {
+	console.log("in add");
+	var p = new product({
+		title:req.body.title,
+		price:req.body.price,
+		instock:req.body.instock,
+		photo:req.body.photo
+    });
 
-	console.log('Logging of request will be done here');
-	next();
+    p.save(function (err) {
+        if (err) {
+            res.send(err);
+        }
+        console.log("added");
+        res.send({ message: 'Product Created !' });
+    });
 });
 
 app.use(cors());
